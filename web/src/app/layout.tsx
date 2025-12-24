5d611d234f48afa9ecbde17837279f7092e09e2c
Baibhab Ghosh <baibhabghosh2003@gmail.com>
2025-12-24 11:05:22 +0530

    feat: add google analytics to web

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,6 +2,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "ExpenseX Dashboard",
@@ -28,6 +29,20 @@ export default function RootLayout({
         {children}
         <Toaster theme="dark" position="top-center" />
         <Analytics />
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-N6HJF7JF90"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-N6HJF7JF90');
+          `}
+        </Script>
       </body>
     </html>
   );
